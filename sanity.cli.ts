63131619248a59d6566b8251327f604cf5d6d41c
Jane Kostov <jane.kostov@expertallies.com>
2026-02-11 23:38:30 +0200

    Initial commit

---
+++ sanity.cli.ts
@@ -0,0 +1,23 @@
+/**
+* This configuration file lets you run `$ sanity [command]` in this folder
+* Go to https://www.sanity.io/docs/cli to learn more.
+**/
+import { defineCliConfig } from 'sanity/cli'
+
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
+
+export default defineCliConfig({
+	api: { projectId, dataset },
+	typegen: {
+		// Scan the app, lib, and components directories for GROQ queries
+		path: [
+			'./app/**/*.{ts,tsx,js,jsx}',
+			'./lib/**/*.{ts,tsx,js,jsx}',
+			'./components/**/*.{ts,tsx,js,jsx}',
+			'./sanity/**/*.{ts,tsx,js,jsx}'
+		],
+		schema: './schema.json',
+		generates: './sanity.types.ts'
+	}
+})
